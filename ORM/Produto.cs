0b9aeeaabf1428652667697d1aab283b05f20e6e
Luis Henrique <iluishenriquebr@gmail.com>
2022-01-08 12:40:12 -0300

    Adição da Classe Produto e seus atributos.

---
+++ ORM/Produto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ORM
+{
+    class Produto
+    {
+        public int Cod_Produto;
+        public string Ddescricao;
+        public float Valor_Unitario;
+    }
+}
