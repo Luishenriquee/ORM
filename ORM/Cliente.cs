94d9c31271b68e3ef00067a4cedad40d25cec749
Luis Henrique <iluishenriquebr@gmail.com>
2022-01-08 12:37:06 -0300

    Adição da Classe Cliente e seus atributos.

---
+++ ORM/Cliente.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ORM
+{
+    class Cliente
+    {
+        public int Cod_Cliente;
+        public string Nome_cliente;
+        public string Endereco;
+    }
+}
