a23adc1fa962d3c8d0f38691a688bfcd66f93009
Luis Henrique <iluishenriquebr@gmail.com>
2022-01-08 12:38:39 -0300

    Adição da classe Vendedor e seus atributos.

---
+++ ORM/Vendedor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ORM
+{
+    class Vendedor
+    {
+        public int Cod_Vendedor;
+        public string Nome_Vendedor;
+        public string Meta_de_Venda;
+    }
+}
